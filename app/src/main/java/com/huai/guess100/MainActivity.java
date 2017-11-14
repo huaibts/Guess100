188165297de376ffb9f8c1f0188a119b698882f7
huaijung Huang <je3rjoy@gmail.com>
2017-11-14 10:10:04 +0800

    findViews

--- app/src/main/java/com/huai/guess100/MainActivity.java
+++ app/src/main/java/com/huai/guess100/MainActivity.java
@@ -2,6 +2,10 @@ package com.huai.guess100;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+
+    }
+    public void findViews(){
+        TextView info = (TextView) findViewById(R.id.info);
+        TextView secret = (TextView) findViewById(R.id.secret);
+        EditText number = (EditText) findViewById(R.id.number);
+        Button send = (Button) findViewById(R.id.send);
     }
 }
