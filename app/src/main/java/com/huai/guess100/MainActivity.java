adfa7b2740e2f5f34bbe69167825593bce73d577
huaijung Huang <je3rjoy@gmail.com>
2017-11-14 10:34:50 +0800

    Guess

--- app/src/main/java/com/huai/guess100/MainActivity.java
+++ app/src/main/java/com/huai/guess100/MainActivity.java
@@ -1,5 +1,6 @@
 package com.huai.guess100;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +13,8 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     Random r = new Random();
     int n = r.nextInt(100)+1;
+    int min = 0;
+    int max = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,12 +25,29 @@ public class MainActivity extends AppCompatActivity {
     public void findViews(){
         TextView info = (TextView) findViewById(R.id.info);
         TextView secret = (TextView) findViewById(R.id.secret);
-
         EditText number = (EditText) findViewById(R.id.number);
         Button send = (Button) findViewById(R.id.send);
+        int sec = Integer.parseInt(secret.getText().toString());
+        int num = Integer.parseInt(number.getText().toString());
 
 
         secret.setText(Integer.toString(n));
+
+        if (num > sec){
+            max = num;
+            new AlertDialog.Builder(this)
+                    .setMessage(min+"to"+max)
+                    .setPositiveButton("OK", null)
+                    .show();
+        }else if (num < sec){
+            min = num;
+            new AlertDialog.Builder(this)
+                    .setMessage(min+"to"+max)
+                    .setPositiveButton("OK", null)
+                    .show();
+        }
+
+
 
     }
 }
